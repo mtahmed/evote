585e4a899b9d748010d556e9f99c9bf2481f9a2d
Muhammad Tauqir Ahmad <muhammad.tauqir.ahmad@gmail.com>
2013-12-07 17:14:15 -0500

    Add auth.py with Auth class to handle authentication.

---
+++ auth.py
@@ -0,0 +1,21 @@
+import os
+
+
+class Auth:
+    '''This class will provide the voter authentication.
+
+    Right now it uses the uWaterloo CAS to provide authentication.
+    '''
+    def __init__(self):
+        self.user_id = os.environ['REMOTE_USER']
+        # The user is denied if auth fails before they get here so the user must
+        # already be authenticated.
+        self.is_authed = True
+
+    def get_user_id(self):
+        return self.user_id
+
+    def logout(self):
+        '''Logout the currently logged in user.
+        '''
+        pass
