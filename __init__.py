d017fb434b820d063a4642bd8245eb1d7918f72f
Samiya Akhtar <samiyaakhtar7@gmail.com>
2013-12-22 15:18:04 -0500

    Move views into views.py

--- __init__.py
+++ __init__.py
@@ -2,33 +2,13 @@ from pyramid.config   import Configurator
 from pyramid.response import Response
 from pyramid.view     import view_config
 
-from . import auth
-
-@view_config(route_name='main', renderer='templates/base.jinja2')
-def main(request):
-    '''View for the main landing page.
-    '''
-    template_data = dict()
-
-    a = auth.Auth(user_id=request.remote_user)
-    template_data['user_id'] = a.get_user_id()
-
-    return template_data
-
-@view_config(route_name='logout')
-def logout(request):
-    '''View for the logout page. Only redirects to the logout page.
-    '''
-    a = auth.Auth(user_id=request.remote_user)
-    response = Response()
-
-    return a.logout(response)
+from . import views
 
 def make_wsgi_app():
     '''This function returns a Pyramid WSGI application.
     '''
     config = Configurator()
-
+    
     # Routes
     config.add_route('main', '')
     config.add_route('logout', '/logout')
